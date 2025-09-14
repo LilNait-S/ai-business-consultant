fd37f40a97b19166c8b74de31f1dbb8af6680159
Nait-s <lilnait.santos@gmail.com>
2025-09-14 17:44:34 -0500

    fix: update import statement for App component to remove file extension

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import './src/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.tsx';
+import App from './app';
 
 
 const rootElement = document.getElementById('root');
